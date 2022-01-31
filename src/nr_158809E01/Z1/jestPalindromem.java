74d996e6531d4eddeeab0a129087e58ebcd99f2c
Maciej <158809@student.uwm.edu.pl>
2022-01-31 16:14:27 +0100

    158809 Egzamin Programowanie Obiektowe 2021/2022 Zima

---
+++ src/nr_158809E01/Z1/jestPalindromem.java
@@ -0,0 +1,14 @@
+package nr_158809E01.Z1;
+
+public class jestPalindromem<T> implements Comparable<T> {
+    private Object[] a;
+
+    public jestPalindromem(int s){
+        a=new Object[s]
+    }
+    T get (int i)
+    {
+        @SuppressWarnings()
+    }
+
+}
---
+++ src/nr_158809E01/Z1/main.java
@@ -0,0 +1,5 @@
+package nr_158809E01.Z1;
+from java.util.ArrayList
+
+public class main {
+}
